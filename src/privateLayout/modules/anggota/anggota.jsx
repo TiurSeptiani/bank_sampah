bbc89381c9e5fd9fada268a69d26aa2cf1f3aebf
Flavianus Ara Pamungkas <ara081200@gmail.com>
2023-11-08 23:32:51 +0700

    Update Component Anggota

--- src/privateLayout/modules/anggota/anggota.jsx
+++ src/privateLayout/modules/anggota/anggota.jsx
@@ -4,40 +4,60 @@ import { useSelector } from "react-redux";
 
 
 function Anggota() {
-	const { data } = useSelector((state) => state.dataNasabah)
-
+	const { data } = useSelector((state) => state.dataNasabah);
+  
+	console.log("DATA", data);
+  
 	const columns = [
-		{
-			title: "No",
-			width: 100,
-			render: (text, record, index) => index + 1,
-		},
-		{
-			title: "Nama Nasabah",
-			dataIndex: "namaLengkap",
-		},
-		{
-			title: "Alamat",
-			dataIndex: "alamat",
-		},
-		{
-			title: "RT",
-			dataIndex: "noRt",
-		},
-		{
-			title: "No. Handphone",
-			dataIndex: "noHp",
-		},
-		{
-			title: "Tanggal Bergabung",
-			dataIndex: "tanggalBergabung",
-		},
+	  {
+		title: "No",
+		width: 100,
+		render: (text, record, index) => index + 1,
+	  },
+	  {
+		title: "Nama Nasabah",
+		dataIndex: "namaLengkap",
+	  },
+	  {
+		title: "Alamat",
+		dataIndex: "alamat",
+	  },
+	  {
+		title: "RT",
+		dataIndex: "noRt",
+	  },
+	  {
+		title: "No. Handphone",
+		dataIndex: "noHp",
+	  },
+	  {
+		title: "Tanggal Bergabung",
+		dataIndex: "tanggalBergabung",
+	  },
 	];
+  
+	// Pisahkan data "Petugas" dan "Anggota"
+	const anggotaData = Object.values(data).filter((item) => item.status === "Nasabah");
+	const petugasData = Object.values(data).filter((item) => item.status === "Petugas");
+  
 	return (
+	  <>
+		{/* List Petugas */}
+		{petugasData.length > 0 && (
+		  <Col>
+			<Divider orientation="left">List Petugas</Divider>
+			<Table columns={columns} dataSource={petugasData} scroll={{ x: 100 }} />
+		  </Col>
+		)}
+  
+		{/* List Anggota */}
 		<Col>
-			<Divider orientation='left'>List Anggota</Divider>
-			<Table columns={columns} dataSource={data ? Object.values(data) : []} scroll={{ x: 100 }} />
+		  <Divider orientation="left">List Anggota</Divider>
+		  <Table columns={columns} dataSource={anggotaData} scroll={{ x: 100 }} />
 		</Col>
+	  </>
 	);
-}
-export default Anggota;
+  }
+  
+  export default Anggota;
+  
