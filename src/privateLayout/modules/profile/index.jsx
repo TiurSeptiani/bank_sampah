e61f5982eb89949ae3b70a16b06fbe80a419c8fa
ara-pamungkas <ara081200@gmail.com>
2023-11-17 23:24:01 +0700

    update

--- src/privateLayout/modules/profile/index.jsx
+++ src/privateLayout/modules/profile/index.jsx
@@ -23,8 +23,11 @@ function Tabungan() {
   const { Title, Text } = Typography;
   const { currentUser } = useSelector((state) => state.auth);
   const { data } = useSelector((state) => state.dataNasabah);
-  const isNabah = Object.values(data).find(
+  const isNasabah = Object.values(data).find(
     (user) => user.status === "Nasabah" && user.uid === currentUser.uid
+  );
+  const isPetugas = Object.values(data).find(
+    (user) => user.status === "Petugas" && user.uid === currentUser.uid
   );
 
   const items = [
@@ -40,53 +43,93 @@ function Tabungan() {
 
   return (
     <Col id="tabungan">
-      {isNabah && (
-        <div id="tabungan-container">
-        <div className="tabungan-header">
-          <div className="tabungan-foto">
-            <Image
-              id="gambar"
-              style={{ borderRadius: "50%" }}
-              src="error"
-              fallback={pic}
-            />
-          </div>
-          <div className="tabungan-nama">
-            <div className="tabungan-nama-container">
-              <span className="tabungan-nama-h1">{isNabah.namaLengkap}</span>
-              <span className="tabungan-nama-h2">
-                Email: {isNabah.email}
-              </span>
+      {isNasabah && (
+        <div id="tabungan-container-nasabah">
+          <div className="tabungan-header">
+            <div className="tabungan-foto">
+              <Image
+                id="gambar"
+                style={{ borderRadius: "50%" }}
+                src="error"
+                fallback={pic}
+              />
             </div>
-            <div className="tabungan-nama-setting">
-              <Dropdown
-                menu={{
-                  items,
-                }}
-                trigger={["click"]}
-              >
-                <a id="setting" onClick={(e) => e.preventDefault()}>
-                  <Space>
-                    <span>
-                      <SettingOutlined />
-                    </span>
-                  </Space>
-                </a>
-              </Dropdown>
+            <div className="tabungan-nama">
+              <div className="tabungan-nama-container">
+                <span className="tabungan-nama-h1">
+                  {isNasabah.namaLengkap}
+                </span>
+                <span className="tabungan-nama-h2">
+                  Email: {isNasabah.email}
+                </span>
+              </div>
+              <div className="tabungan-nama-setting">
+                <Dropdown
+                  menu={{
+                    items,
+                  }}
+                  trigger={["click"]}
+                >
+                  <a id="setting" onClick={(e) => e.preventDefault()}>
+                    <Space>
+                      <span>
+                        <SettingOutlined />
+                      </span>
+                    </Space>
+                  </a>
+                </Dropdown>
+              </div>
+            </div>
+          </div>
+          <div id="tabungan-body">
+            <span className="saldo-tabungan-nama">Saldo Tabungan</span>
+            <span className="saldo-tabungan">
+              <h6 className="rupiah">Rp</h6>
+              <h1 className="saldo">{isNasabah.saldo}</h1>
+            </span>
+          </div>
+        </div>
+      )}
+
+      {isPetugas && (
+        <div id="tabungan-container-petugas">
+          <div className="tabungan-header">
+            <div className="tabungan-foto">
+              <Image
+                id="gambar"
+                style={{ borderRadius: "50%" }}
+                src="error"
+                fallback={pic}
+              />
+            </div>
+            <div className="tabungan-nama">
+              <div className="tabungan-nama-container">
+                <span className="tabungan-nama-h1">
+                  {isPetugas.namaLengkap}
+                </span>
+                <span className="tabungan-nama-h2">
+                  Email: {isPetugas.email}
+                </span>
+              </div>
+              <div className="tabungan-nama-setting">
+                <Dropdown
+                  menu={{
+                    items,
+                  }}
+                  trigger={["click"]}
+                >
+                  <a id="setting" onClick={(e) => e.preventDefault()}>
+                    <Space>
+                      <span>
+                        <SettingOutlined />
+                      </span>
+                    </Space>
+                  </a>
+                </Dropdown>
+              </div>
             </div>
           </div>
         </div>
-        <div id="tabungan-body">
-          <span className="saldo-tabungan-nama">Saldo Tabungan</span>
-          <span className="saldo-tabungan"
-          >
-            <h6 className="rupiah">Rp</h6>
-            <h1 className="saldo">
-              {isNabah.saldo}
-            </h1>
-          </span>
-        </div>
-      </div>
       )}
     </Col>
   );
