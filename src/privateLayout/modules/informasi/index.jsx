c556e2dca3df8a4466b611531a55b8b99c79ba2b
ara-pamungkas <ara081200@gmail.com>
2023-11-21 01:33:12 +0700

    update

--- src/privateLayout/modules/informasi/index.jsx
+++ src/privateLayout/modules/informasi/index.jsx
@@ -1,17 +1,48 @@
 import React from "react";
 import sampul from "../../../assets/sampul.jpg";
 import struktur from "../../../assets/struktur.jpg";
-import { Col, Image } from "antd";
+import { Collapse, Image } from "antd";
+
+const { Panel } = Collapse;
 
 function Informasi() {
+
+  const items = [
+    {
+      key: '1',
+      label: 'Sampul',
+      children: (
+        <>
+          <Image id="gambar-informasi-satu" src={sampul} alt="Sampul" />
+          <p>
+            Sampul dari Bank Sampah Anugerah 04 Klitren.
+          </p>
+        </>
+      ),
+    },
+    {
+      key: '2',
+      label: 'Struktur Bank Sampah',
+      children: (
+        <>
+          <Image id="gambar-informasi-dua" src={struktur} alt="Struktur" />
+          <p>
+            Ini merupakan struktur dari Bank Sampah Anugerah 04 Klitren.
+          </p>
+        </>
+      ),
+    }
+  ];
+
   return (
-    <div style={{display: "flex", flexDirection: "column", justifyContent: "center", gap: "20px", alignItems: "center", flexWrap: "wrap", width: "100%"}}>
-      <Col>
-        <Image width={500} src={sampul} />
-      </Col>
-      <Col>
-        <Image width={500} src={struktur} />
-      </Col>
+    <div>
+      <Collapse defaultActiveKey={['1']}>
+        {items.map((item) => (
+          <Panel key={item.key} header={item.label}>
+            {item.children}
+          </Panel>
+        ))}
+      </Collapse>
     </div>
   );
 }
--- src/privateLayout/pages/informasi/index.jsx
+++ src/privateLayout/pages/informasi/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Informasi from '../../modules/informasi'
+import "../../../styles/informasi/informasi.css"
 
 function Index() {
   return (
